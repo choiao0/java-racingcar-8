54ac19eeb1ad04c40e448994fdcbeb0cb554800a
choiao0 <qawse6@daum.net>
2025-10-27 14:24:18 +0900

    refactor(InputView): 메서드 단위를 작게 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,18 +4,39 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String getCarNames() {
-        return Console.readLine();
+        String input = readInput();
+        return trimInput(input);
     }
 
     public int getAttempts() {
-        String attempts = Console.readLine();
-        validateAttempts(attempts);
-        return Integer.parseInt(attempts);
+        String input = Console.readLine();
+        String trimmedInput = trimInput(input);
+        validateAttempts(trimmedInput);
+        return convertToInt(trimmedInput);
+    }
+
+    private String readInput() {
+        return Console.readLine();
+    }
+
+    private String trimInput(String input) {
+        return input.trim();
     }
 
     private void validateAttempts(String attempts) {
-        if (attempts.trim().isEmpty() || !attempts.matches("^[1-9][0-9]*$")) {
+        if (attempts.isEmpty()) {
+            throw new IllegalArgumentException("시도 횟수는 공백일 수 없습니다.");
+        }
+        if (!isPosiveNumber(attempts)) {
             throw new IllegalArgumentException("시도 횟수는 양수여야 합니다.");
         }
+    }
+
+    private boolean isPosiveNumber(String number) {
+        return number.matches("^[1-9][0-9]*$");
+    }
+
+    private int convertToInt(String attempts) {
+        return Integer.parseInt(attempts);
     }
 }
